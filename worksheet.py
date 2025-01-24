2fa6edcb8ab5b1b164d56204730a46c74849082e
Lyna Omari <lyna.omari@etu-upsaclay.fr>
2025-01-24 16:36:30 +0000

    nouveau fichier

---
+++ worksheet.py
@@ -0,0 +1,23 @@
+from modules.open_digraph import *
+
+# Graphe quelconque
+
+n0 = node(0, 'a', {3:1 , 2:1}, {1:1, 2:1})
+n1 = node(1, 'b', {0:1}, {1:1 , 5:1})
+n2 = node(2, 'c', {0:1 , 1:2}, {0:1})
+i0 = node(3, "i0",{},{0:1})
+i1 = node(4, "i1",{},{0:1})
+o0 = node(5,"o0",{1:1},{})
+o1 = node(6,"o1",{2:1},{})
+d0 = open_digraph([3,4], [5,6], [n0,n1,n2,i0,i1,o0,o1])
+
+# Affichage node
+
+print(n0)
+
+# Affichage graphe
+
+print(d0)
+
+d1 = open_digraph.empty()
+print(d1)
